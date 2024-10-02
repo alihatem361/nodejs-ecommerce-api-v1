c2ef785cdc15a26c8080d8b3c9fb03b51d52845b
Ali Hatem <alihatemramadan5@gmail.com>
2024-10-02 15:56:58 +0300

    Refactor code structure and handle unknown routes with a custom error class

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import logger from "./middlewares/logger.js";
-
+import ApiError from "./utils/ApiError.js";
+import e from "express";
 dotenv.config();
 
 const app = express();
@@ -24,13 +25,19 @@ app.use("/api/v1", userRoutes);
 app.use("/api/v1", categoryRoutes);
 
 app.all("*", (req, res, next) => {
-  const error = new Error(`Route not found - ${req.originalUrl}`);
-  next(error);
+  next(new ApiError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
 // Error handler
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: err.message });
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
 });
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
---
+++ utils/ApiError.js
@@ -0,0 +1,12 @@
+// utils/ApiError.js
+// @desc    Custom error class for handling errors in the application error handler
+class ApiError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.isOperational = true;
+  }
+}
+
+export default ApiError;
