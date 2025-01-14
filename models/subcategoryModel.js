30dbe9d2ea05cb2b9e7c7fb12b2a585dd227dc07
Ali Hatem <alihatemramadan5@gmail.com>
2025-01-14 12:58:17 +0200

    Add subcategory model and trim slug fields in category model

---
+++ models/subcategoryModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const subcategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter a subcategory name"],
+      unique: [true, "Subcategory name must be unique"],
+      trim: true,
+      maxLength: [50, "Subcategory name cannot exceed 50 characters"],
+      minlength: [3, "Subcategory name must be at least 3 characters long"],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+      trim: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: [true, "Please provide a category ID"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const SubcategoryModel = mongoose.model("Subcategory", subcategorySchema);
+
+export default SubcategoryModel;
